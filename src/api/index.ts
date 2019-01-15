664a38ec1893272d1bf1234c034f16b9727d696f
Smithoo <smithoo7646@gmail.com>
2019-01-15 17:14:50 +0900

    refact: remove onNot2xx

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,14 +4,3 @@ export const baseServerURL = 'http://localhost:8080';
 export function is2xx(response: Response): boolean {
     return response.status >= 200 && response.status < 300;
 }
-
-export async function onNot2xx(response: Response) {
-    let errorCode;
-    try {
-        const json = await response.json();
-        errorCode = json.errorCode;
-    } catch (err) {
-        throw response.status;
-    }
-    throw errorCode;
-}
