c359fcd322c98b213de88b524f620d428de6d5d7
skatpgusskat <skatpgusskat@naver.com>
2019-01-16 23:44:24 +0900

    Connect humor board with server

--- src/stores/board/index.ts
+++ src/stores/board/index.ts
@@ -1,15 +1,17 @@
 import { BoardState, BoardPostState } from './types';
+import { fetchGraphQL } from '@/api/fetchGraphQL';
 
-const board: BoardState = {
+const boardState: BoardState = {
     totalCount: 0,
     currentPage: 0,
     type: '',
-    posts: []
+    posts: [],
+    boardName: '',
 };
 
 export default {
     namespaced: true,
-    state: board,
+    state: boardState,
     getters: {
         totalCount(state: BoardState): number {
             return state.totalCount;
@@ -22,7 +24,10 @@ export default {
         },
         posts(state: BoardState): BoardPostState[] {
             return state.posts;
-        }
+        },
+        boardName(state: BoardState): string {
+            return state.boardName;
+        },
     },
     mutations: {
         setBoardType(state: BoardState, type) {
@@ -32,38 +37,43 @@ export default {
             state.currentPage = page;
         },
         setPosts(state: BoardState, posts: BoardPostState[]) {
+            console.log(posts);
             state.posts = posts;
+        },
+        setBoardName(state: BoardState, boardName: string) {
+            state.boardName = boardName;
         }
     },
     actions: {
-        async fetch({ commit }, { type, page }) {
+        async fetch({ commit }, { boardName, type, page }) {
             commit('setBoardType', type);
             commit('setCurrentPage', page);
-            
-            // const boardData = await api.fetchBoard(boardName, index, 20);
-            const boardData = {
-                posts: [
-                    {
-                        boardId: 123123,
-                        id: 'post-1',
-                        title: 'hellasdfsdaf asdf asdf as dasdd fsad asd asd fdsafasd sad adsff o, world',
-                        thumbnail: 'http://i.huv.kr:8080/c1f6b1dd6f.jpg?c=2',
-                        contentS3Key: '',
-                        writer: {
-                            id: 1,
-                            username: 'smithoasdf asdf sad fsad fasd asdfo',
-                            thumbnail: 'http://i.huv.kr:8080/b3aab0c5bcbcb4e7c7dfc0bd.jpg?c=2'
-                        },
-                        createdAt: '2019-01-07 12:21',
-                        read: 12313,
-                        likes: 10,
-                        isLiked: true,
-                        commentCount: 3
+            commit('setBoardName', boardName)
+
+            const query = `{
+                board(name: "${boardName}") {
+                    id
+                    posts(page: 0, pageSize: 10) {
+                        id
+                        title
+                        thumbnailUrl
+                        contentS3Key
+                        writer {
+                            id
+                            username
+                            avatarUrl
+                        }
+                        createdAt
+                        isViewed
+                        views
+                        likes
+                        isLiked
+                        commentCount
                     }
-                ]
-            };
-            
-            commit('setPosts', boardData.posts);
+                }
+            }`;
+            const { board } = await fetchGraphQL(query);
+            commit('setPosts', board.posts);
         }
     }
 };
