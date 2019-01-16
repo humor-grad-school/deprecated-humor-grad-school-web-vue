c9e63ae56b67866113d9179cbfebf43c0fbc251a
skatpgusskat <skatpgusskat@naver.com>
2019-01-16 23:43:49 +0900

    Add fetch graphql function

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,13 @@ import App from './App.vue';
 import router from './router';
 import store from './stores';
 import { HgsRestApi } from './api/types/generated/client/ClientApis';
+import { setGrahQLBaseServerUrl } from './api/fetchGraphQL';
 
 Vue.config.productionTip = false;
 
-HgsRestApi.setBaseServerUrl('http://localhost:8080');
+const baseServerUrl = 'http://localhost:8080';
+HgsRestApi.setBaseServerUrl(baseServerUrl);
+setGrahQLBaseServerUrl(baseServerUrl);
 
 new Vue({
   router,
