b0e40009e4085946e68e8ee9585e020123fbf6c9
skatpgusskat <skatpgusskat@naver.com>
2019-01-18 21:08:07 +0900

    Update google sign out function

--- src/modules/google-auth.ts
+++ src/modules/google-auth.ts
@@ -14,7 +14,14 @@ export async function initGoogle({ success, error }) {
         });
     }
 
-    await signOutGoogle();
+    await new Promise(async (resolve) => {
+        gapi.auth2.init({
+            client_id: '74489406824-dlmplsl075187spamd9a4m6g90ah56so.apps.googleusercontent.com',
+        }).then(async (googleAuth) => { // this is not promise.
+            await googleAuth.signOut();
+            resolve();
+        });
+    });
 
     gapi.signin2.render('google-log-in', {
         scope: 'profile email',
@@ -31,7 +38,8 @@ export async function initGoogle({ success, error }) {
 
 export async function signOutGoogle() {
     const instance = gapi.auth2.getAuthInstance();
-    if (instance && instance.isSignedIn) {
+    const isSignedIn = instance && instance.isSignedIn.get();
+    if (isSignedIn) {
         await instance.signOut();
     }
 }
